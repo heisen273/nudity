4f57a00663e58627c595068ce732cfa7ca47d67b
heisen273 <f0xkid@ukr.net>
2020-06-18 10:36:14 +0200

    Update __init__.py
    use tfv1 instead of 2.0

--- nudity/__init__.py
+++ nudity/__init__.py
@@ -3,9 +3,10 @@ from __future__ import division
 from __future__ import print_function
 import os
 import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 import requests
 
+tf.disable_v2_behavior()
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 class Nudity:
